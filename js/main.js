ab07bd2a3d76987383bb3feb39bfe26fc5c71635
BoriskaGlebov <152175264+BoriskaGlebov@users.noreply.github.com>
2024-03-23 19:07:50 +0300

    Update main.js remove scripts

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 $(function(){
 
-    let showPopup = function(){
-        $('.popup-container').css('display', 'flex');
-    };
-
-    let closePopup = function(){
-        $('.popup-container').css('display', 'none');
     };
 
     let clearForm = function(){
